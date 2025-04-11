0a201990de74c3b302cb53380b6c1ad63d41420a
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-11 17:32:47 +0530

    added slider to testimonial

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -9,5 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './testimonial.component.css'
 })
 export class TestimonialComponent {
+  private currentPosition = 0;
 
+  slideTestimonials(direction: 'left' | 'right') {
+    const testimonialCard = document.querySelector('.testimonial-card') as HTMLElement;
+    if (!testimonialCard) return;
+
+    if (direction === 'right' && this.currentPosition > -50) {
+      this.currentPosition -= 50;
+    } else if (direction === 'left' && this.currentPosition < 0) {
+      this.currentPosition += 50;
+    }
+
+    testimonialCard.style.transform = `translateX(${this.currentPosition}%)`;
+  }
 }
