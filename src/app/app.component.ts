8d2ad05f2c2e600f6629d0d34bbf9f78f23b8485
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-11 16:13:21 +0530

    completed dev testimonial-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,22 @@ import { AboutComponent } from './components/about/about.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MoreworkComponent } from "./components/morework/morework.component";
+import { TestimonialComponent } from "./components/testimonial/testimonial.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent, BannerComponent, ServicesComponent, AboutComponent, FooterComponent, MoreworkComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NavigationComponent,
+    BannerComponent,
+    ServicesComponent,
+    AboutComponent,
+    FooterComponent,
+    MoreworkComponent,
+    TestimonialComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
