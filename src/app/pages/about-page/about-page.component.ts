2acaf6192dcc529b784a49f2f1c8096382ea5849
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-29 13:08:31 +0530

    update

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { NavigationComponent } from '../../components/navigation/navigation.component';
-import { AboutComponent } from '../../components/about/about.component';
-import { TeamComponent } from '../../components/team/team.component';
-import { TestimonialComponent } from '../../components/testimonial/testimonial.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 import { AboutBannerComponent } from '../../components/about-banner/about-banner.component';
 
 @Component({
@@ -14,7 +10,6 @@ import { AboutBannerComponent } from '../../components/about-banner/about-banner
     HeaderComponent,
     NavigationComponent,
     AboutBannerComponent,
-    FooterComponent
   ],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
